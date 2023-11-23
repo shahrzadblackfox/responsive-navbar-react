0e814ac0b9b3930789b044971abce975d585b2bb
Shahrzad <75183431+shahrzadblackfox@users.noreply.github.com>
2023-11-24 02:01:31 +0330

    adding hamburger menu

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,46 +1,60 @@
-import React from 'react'
-import logo from './logo.png';
-import { FaBars, FaTwitter, FaTimes } from 'react-icons/fa';
-import {links, socials} from './data';
+import React, { useEffect, useRef, useState } from "react";
+import logo from "./logo.png";
+import { FaBars, FaTwitter, FaTimes } from "react-icons/fa";
+import { links, socials } from "./data";
 function Navbar() {
-     return (
-          <nav>
-               <div className="container">
-                    <div className="nav-logo">
-                         <button className="nav-toggle">
-                              <FaBars />
-                         </button>
-                         <img src={logo} alt="" />
-                    </div>
-                    <div className="nav-links">
-                         <ul className="list">
-                         {
-                              links.map((link) => {
-                                   const {id,url,text} = link;
-                                   return (
-                                        <li key={id}>
-                                             <a href={url}>{text}</a>
-                                        </li>
-                                   )
-                              })
-                         }
-                         </ul>
-                    </div>
-                    <ul className="nav-social">
-                         {
-                              socials.map((social) => {
-                                   const {id,url,icon} = social;
-                                   return (
-                                        <li key={id}>
-                                             <a href={url}>{icon}</a>
-                                        </li>
-                                   )
-                              })
-                         }
-                    </ul>
-               </div>
-          </nav>
-     )
+  const [showLink, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const linkHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLink) {
+      linksContainerRef.current.style.height = `${linkHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [showLink]);
+
+  return (
+    <nav>
+      <div className="container">
+        <div className="nav-logo">
+          <button
+            className="nav-toggle"
+            onClick={() => setShowLinks(!showLink)}
+          >
+            {showLink ? <FaTimes /> : <FaBars />}
+          </button>
+          <img src={logo} alt="" />
+        </div>
+
+        <div className="nav-links" ref={linksContainerRef}>
+          <ul className="list" ref={linksRef}>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+
+        <ul className="nav-social">
+          {socials.map((social) => {
+            const { id, url, icon } = social;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
